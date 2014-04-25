3d2d4159cdc7d5e1a78005d5574cab21acf61875
shinolife63 <shinolife63@gmail.com>
2014-04-25 16:55:04 -0400

    Code overhaul 1
    -Added DYNAMICALLY DEFINABLE FUNCTIONS
    -Redid the lexical analysis engine
    -User input is now handled as a data type
    -Uses tail recursion between the lexer and parser to keep things nice
    and tidy
    -New label and go methods
    -Added basic file i/o
    -Added delay method
    -added a bunch of other shit I dont feel like listing

--- rainbows.l.py
+++ rainbows.l.py
@@ -6,90 +6,106 @@ data_types = {
     '$':'string',
     '%':'integer',
     '@':'variable',
+    '_':'input',
 }
+stack = deque([0,0,0])
 flags = {'ifstat':0,'moveahead':1,'pointer':0,'setmode':0,'back':0,}
 variables = {}
+arguments = []
+functions = {}
 lines = code.split('\n')
 error = lambda line: print('"%s" Contained Error'%line)
 label = lambda var: var.replace('@','')
 def data(arg):
+    if type(arg)==list:
+        arg = ' '.join(arg)
     if Type(arg)=='string':
         if flags['setmode']: return arg
-        else: return str(arg[1:])
+        else: return str(arg[1:]).replace('>n','\n').replace('>t','\t').replace('>:',';')
     if Type(arg)=='integer':
         if flags['setmode']: return arg
         else: return int(arg[1:])
     if Type(arg)=='variable':
-        return data(variables[label(arg)])
+        return data(variables[arg[1:]])
+    if Type(arg)=='input':
+        return data('%s%s'%(arg[1],input(arg[3:arg.index(']')])))
+    
 def Type(data):
-    try: return data_types[data[0]]
-    except: print('Unkown data type %s'%data[0])
+    try: return [data_types[prefix] for prefix in data_types if data.startswith(prefix)][0]
+    except: pass
     
 def evaluate(line):
     #print('\t'+line) # for debugging
     for subline in line.split(';'):
         flags['moveahead']=1
         tokens = subline.split(' ')
-        try:
-            command = tokens[0]
-            if command.startswith('-'): command,arg1 = 'goto',command[1:]
-        except: pass
-        try: command,arg1 = tokens[0],tokens[1]
-        except: pass
-        try: comand,arg1,arg2 = tokens[0],tokens[1],tokens[2]
-        except: pass
-        try: comand,arg1,arg2,arg3 = tokens[0],tokens[1],tokens[2],tokens[3]
-        except: pass
-        if command == 'set':
-            if Type(arg1)!='variable':
-                error(line)
-            else:
-                flags['setmode']=1
-                variables[label(arg1)]=data(arg2)
-                flags['setmode']=0
-        if command == 'add':
-            if Type(arg3)!='variable':
+        
+        if tokens[0] == 'set':
+            flags['setmode']=1
+            variables[label(tokens[1])]=data(tokens[2:])
+            flags['setmode']=0
+        if tokens[0] == 'add':
+            if Type(tokens[3])!='variable':
                     error(line)
             else:
-                variables[label(arg3)]='%'+'%d'%(data(arg1)+data(arg2))
-        if command == 'sub':
-            if Type(arg3)!='variable':
+                variables[label(tokens[3])]='%'+'%d'%(data(tokens[1])+data(tokens[2]))
+        if tokens[0] == 'sub':
+            if Type(tokens[3])!='variable':
                     error(line)
             else:
-                variables[label(arg3)]='%'+'%d'%(data(arg1)-data(arg2))
-        if command == 'mult':
-            if Type(arg3)!='variable':
+                variables[label(tokens[3])]='%'+'%d'%(data(tokens[1])-data(tokens[2]))
+        if tokens[0] == 'mult':
+            if Type(tokens[3])!='variable':
                     error(line)
             else:
-                variables[label(arg3)]='%'+'%d'%(data(arg1)*data(arg2))
-        if command == 'div':
-            if Type(arg3)!='variable':
+                variables[label(tokens[3])]='%'+'%d'%(data(tokens[1])*data(tokens[2]))
+        if tokens[0] == 'div':
+            if Type(tokens[3])!='variable':
                     error(line)
             else:
-                variables[label(arg3)]='%'+'%d'%(data(arg1)/data(arg2))
-        if command == 'disp':
-             stdout.write(str(data(arg1)).replace('\\\\','\n').replace('\\',' '))
-        if command == 'input':
-            if Type(arg1)!='variable': error(line)
-            else: variables[label(arg1)]=input('')
-        if command == 'if':
-            if arg2 == '=':
-                if data(arg1)==data(arg3): flags['ifstat']=1
+                variables[label(tokens[3])]='%'+'%d'%(data(tokens[1])/data(tokens[2]))
+        if tokens[0] == 'disp':
+             stdout.write(str(data(' '.join(tokens[1:])))+'\n')
+        if tokens[0] == 'if':
+            if tokens[2] == '=':
+                if data(tokens[1])==data(tokens[3]): flags['ifstat']=1
                 else: flags['ifstat']=0
-            elif arg2=='!=':
-                if data(arg1)!=data(arg3): flags['ifstat']=1
+            elif tokens[2]=='!=':
+                if data(tokens[1])!=data(tokens[3]): flags['ifstat']=1
                 else: flags['ifstat']=0
             else: error(line)
-        if command == 'then':
+        if tokens[0] == 'then':
             if flags['ifstat']==1: evaluate(' '.join(tokens[1:]))
-        if command == 'else':
+        if tokens[0] == 'else':
             if flags['ifstat']==0: evaluate(' '.join(tokens[1:]))
-        if command == 'jump':
-            flags['pointer'] = data(arg1)-1
-        if command=='goto': flags['pointer'],flags['back']=parsedcode.index('lbl %s'%arg1),flags['pointer']
-        if command=='end': flags['pointer']=flags['back']
-        if command=='delay': time.sleep(data(arg1))
+        if tokens[0] == 'jump':
+            flags['pointer'] = data(tokens[1])-1
+        if tokens[0]=='go': flags['pointer'],flags['back']=parsedcode.index('.%s'%tokens[1]),flags['pointer']
+        if tokens[0]=='end': flags['pointer']=flags['back']
+        if tokens[0]=='delay': time.sleep(data(tokens[1]))
+        if tokens[0]=='inc': variables[Id(token[1])]+=1
+        if tokens[0]=='dec': variables[Id(token[1])]-=1
+        if tokens[0]=='func':
+            functions[tokens[1]]={'number_of_arguments':data(tokens[2]),'expression':' '.join(tokens[3:]).replace('->',';')}
+        if tokens[0]=='call':
+            expr = functions[tokens[1]]['expression']
+            for i in range(functions[tokens[1]]['number_of_arguments']):
+                expr = expr.replace('|%d'%i,str(data(' '.join(tokens[2:]).split(',')[i])))
+            evaluate(expr)
+        if tokens[0]=='read':
+            try: variables[label(tokens[2])]='$%s'%open(data(tokens[1]),'r').read()
+            except: pass
+        if tokens[0]=='write':
+            try: open(data(tokens[1]),'w').write(data(' '.join(tokens[2:]))).close()
+            except: pass
+        if tokens[0]=='stack':
+            if tokens[1]=='push': stack.append(' '.join(tokens[2:]))
         
+ 
+        
+        
+            
+            
 parsedcode = [line[:line.index('`')-1] if '`' in line else line for line in code.split('\n')]
 while flags['pointer']<len(parsedcode):
     evaluate(parsedcode[flags['pointer']])
